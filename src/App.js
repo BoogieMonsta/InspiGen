34a8062254b9f679bb6f4e71ba212e63c2c05bc1
boogiemonsta <boogiemonsta@gmail.com>
2022-05-23 22:03:15 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<QuoteBox />
+		</div>
+	);
 }
 
 export default App;
+
+class QuoteBox extends React.Component {
+
+
+
+	render() {
+		return (
+			<div id='quote-box'>
+				<Quote />
+				<a id='tweet-quote' href='https://twitter.com/intent/tweet'>
+					Tweet it
+				</a>
+			</div>
+		);
+	}
+}
+
+class Quote extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: '"Patience and impatience are the root of all evil."',
+			author: 'Mama Panda',
+		};
+	}
+
+  nextQuote = () => {
+    // TODO: switch quote
+    this.setState(() => ({
+      text: '"We have pates, we have riz."',
+      author: 'Babati'
+    }));
+  };
+
+	render() {
+		return (
+			<div id='quote-wrap'>
+				<p id='text'>{this.state.text}</p>
+				<p id='author'>{this.state.author}</p>
+				<button id='new-quote' onClick={this.nextQuote}>Next Quote</button>
+			</div>
+		);
+	}
+}
