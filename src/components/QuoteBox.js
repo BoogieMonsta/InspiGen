1a9305665b6a18db25ce6950faa3c6a7ef1e572e
boogiemonsta <boogiemonsta@gmail.com>
2022-05-28 10:32:54 +0200

    next quote click fix

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -7,16 +7,11 @@ import { Next } from './Next.js';
 export function QuoteBox(props) {
 
 	const [displayBtns, setDisplayBtns] = useState('hidden');
-	// const [index, setIndex] = useState(Math.floor(Math.random() * quotes.length));
 	const handleMouseEnter = () => {
 		setDisplayBtns('visible');
 	};
 	const handleMouseLeave = () => {
 		setDisplayBtns('hidden');
-	};
-	const nextQuote = () => {
-		// setIndex(Math.floor(Math.random() * quotes.length));
-		console.log(props.onNext);
 	};
 
 	return (
@@ -24,11 +19,10 @@ export function QuoteBox(props) {
 			id='quote-box'
 			onMouseEnter={handleMouseEnter}
 			onMouseLeave={handleMouseLeave}
-			onClick={props.onNext}
 		>
 			<Quote text={quotes[props.index].text} author={quotes[props.index].author} />
 			<Twitter visib={displayBtns} />
-			<Next visib={displayBtns} trigger={nextQuote}/>
+			<Next visib={displayBtns} trigger={props.onNext}/>
 		</div>
 	);
 }
