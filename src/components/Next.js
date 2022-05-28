58bf887b2c68761eb21dc7594184ec9b787d8247
boogiemonsta <boogiemonsta@gmail.com>
2022-05-28 12:36:51 +0200

    cleanup

--- src/components/Next.js
+++ src/components/Next.js
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 
 export const Next = props => {
 	return (
-		<Button className={props.visib} onClick={props.trigger}>
+		<Button
+			className={props.visib}
+			onClick={props.trigger}
+		>
 			<Icon icon='ic:baseline-navigate-next' fontSize={50} color={'white'} />
 		</Button>
 	);
@@ -11,13 +14,10 @@ export const Next = props => {
 
 const Button = styled.button`
 	width: fit-content;
-	font-weight: bold;
 	border: none;
 	background: none;
 	color: #c2c2c2;
 	padding: 1em;
-	text-transform: uppercase;
-	letter-spacing: 0.05em;
 	position: sticky;
 	bottom: 0;
 	left: 100%;
